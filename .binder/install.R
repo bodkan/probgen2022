398ddb8ff58079a4736cd6531123af6141366bd0
Martin Petr <git@bodkan.net>
2022-03-27 17:21:05 +0200

    Install dependencies

--- .binder/install.R
+++ .binder/install.R
@@ -1,6 +1,11 @@
 # 2022-03-24 17:47
 
+# set up the slendr package for Binder
 install.packages("rgdal")
 devtools::install_github("bodkan/slendr")
 
+# configure a dedicated Python environment for slendr
 slendr::setup_env(agree = TRUE)
+
+# packages used in the examples
+install.packages(c("cowplot", "forcats"))
